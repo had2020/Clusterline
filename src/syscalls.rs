d82d660b74ba5af2c40fdc7a4e1e7b51d59ae9cf
had2020 <hadrian.lazic@gmail.com>
2026-01-19 20:17:19 -0800

    updates to trap handler

--- src/syscalls.rs
+++ src/syscalls.rs
@@ -1,1 +1,15 @@
+use core::arch::asm;
+
+pub enum Trap<I, E> {
+    Interrupt(I),
+    Exception(E),
+}
+
+#[no_mangle]
+pub extern "C" fn trap_handler() {
+    #[cfg(target_arch = "riscv32")]
+    {
+        asm!()
+    }
+}
 
