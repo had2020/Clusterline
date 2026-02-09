937a98b1f2ec066aa2c73a8442be07ef898c9cd1
had2020 <hadrian.lazic@gmail.com>
2026-02-08 23:54:23 -0800

    updates

--- src/fs/vfs.rs
+++ src/fs/vfs.rs
@@ -23,19 +23,17 @@ pub struct Inode {
     pub filename_packed: u64,
     pub file_size: usize,
     pub data_blocks_locat: usize,
-    pub parent_idx: u32,      // index in the global array
-    pub first_child_idx: u32, // for directory traversal
-    pub next_sibling_idx: u32,// for directory traversal
-    pub data_region: u64,     // start block on disk
+    pub parent_idx: u32,       // index in the global array
+    pub first_child_idx: u32,  // for directory traversal
+    pub next_sibling_idx: u32, // for directory traversal
+    pub data_region: u64,      // start block on disk
     pub size: u32,
 }
 
-pub struct RootFileTree<const BLOCK_SIZE: usize> {
+pub struct RootFileTree {
     pub page_start: usize,
 }
 
-impl RootFileTree<const BLOCK_SIZE: usize> {
-    pub fn open(*&mut Dentry, cd: u16) {
-        let mut file_ptr: *&mut 
-    }  
+impl RootFileTree {
+    pub fn open() {}
 }
